0fd8f07cde02d983280fd58beb8d19342f4729e8
Mahmoud Abdelaty <126201317+Mahmoud-Abdelaty@users.noreply.github.com>
2024-02-04 22:12:15 +0200

    Final Update
    add Styles and Manage Responsive

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -14,7 +14,7 @@ export default function Intro() {
     <div className="intro" id="intro">
       <div className="left">
         <div className="imgContainer">
-          <img src="assets/me.png" alt="" />
+          <img src="assets/man.png" alt="" />
         </div>
       </div>
       <div className="right">
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,6 +1,31 @@
 import React from "react";
-import "./testimonials.css";
+import "./testimonials.scss";
 
 export default function Testimonials() {
-  return <div className="testimonials" id="testimonials"></div>;
+  return (
+    <div className="testimonials" id="testimonials">
+      <h1>testimonials</h1>
+      <div className="container">
+        <div className="card">
+          <div className="top">
+            <img src="assets/right-arrow.png" className="left" alt="" />
+            <img
+              className="user"
+              src="https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
+            />
+            <img className="right" src="assets/youtube.png" alt="" />
+          </div>
+          <div className="center">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus
+            expedita, veniam placeat iste officia.
+          </div>
+          <div className="bottom">
+            <h3>Alex</h3>
+            <h4>CEO of LAMA</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
